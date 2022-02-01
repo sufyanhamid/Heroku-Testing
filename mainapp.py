b78947bf2b7337e5c2cca127929e41ca68223c56
sufyanhamid <35563027+sufyanhamid@users.noreply.github.com>
2022-02-01 15:45:47 +0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from lxml import etree
 import requests
 from time import sleep
+import pandas as pd
 
 A=0
 while A==0:
@@ -20,5 +21,7 @@ while A==0:
             Temp_List_1.append((Coin_MarkerCap.xpath('//*[@id="__next"]//tbody/tr['+str(i+1)+']/td[3]/a[1]/div[1]/div[1]/p')[0].text).replace('\n',''))
             print(Temp_List_1[i])
             url1 = 'https://api.telegram.org/bot5179356592:AAFJQqOMJkMocIH0SU5J4GnmfhK6EE-_tno/sendMessage?chat_id=5050252731&parse_mode=MarkdownV2&&text=Program 2 New Coin Arrived at Coin Gecko\n'+str(Temp_List_1[i])
-            requests.get(url1)
-            sleep(1)
+            # requests.get(url1)
+            # sleep(1)
+
+        pd.DataFrame(Temp_List_1).to_csv('Tempp.csv')

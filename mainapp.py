704870996a438bd7334041ea00bd69fb5988c248
sufyanhamid <35563027+sufyanhamid@users.noreply.github.com>
2022-02-01 15:19:28 +0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
 from lxml import etree
 import requests
+from time import sleep
 
-HEADERS = ({'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-            (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
-            'Accept-Language': 'en-US, en;q=0.5'})
+A=0
+while A==0:
+    A+=1
+    Count=0
+    if Count==0:
 
-URL_2 = 'https://api.telegram.org/bot5179356592:AAFJQqOMJkMocIH0SU5J4GnmfhK6EE-_tno/sendMessage?chat_id=5050252731&parse_mode=MarkdownV2&&text=Program 3 New Coin Arrived at Coin Gecko'
+        URL_1='https://coinmarketcap.com/new/'
+        webpage1 = requests.get(URL_1, headers=HEADERS)
+        soup1 = BeautifulSoup(webpage1.content, "html.parser")
+        Coin_MarkerCap = etree.HTML(str(soup1))
 
-requests.get(URL_2)#, headers=HEADERS)
-print(URL_2)
+        Temp_List_1=[]
+
+        for i in range(0, 5):
+            Temp_List_1.append((Coin_MarkerCap.xpath('//*[@id="__next"]//tbody/tr['+str(i+1)+']/td[3]/a[1]/div[1]/div[1]/p')[0].text).replace('\n',''))
+            print(Temp_List_1[i])
+            url1 = 'https://api.telegram.org/bot5179356592:AAFJQqOMJkMocIH0SU5J4GnmfhK6EE-_tno/sendMessage?chat_id=5050252731&parse_mode=MarkdownV2&&text=Program 2 New Coin Arrived at Coin Gecko\n'+str(Temp_List_1[i])
+            requests.get(url1)
+            sleep(1)
